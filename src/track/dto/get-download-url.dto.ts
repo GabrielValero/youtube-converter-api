49ddf2d55a3ee4bc90fedaade19dfb6c35c0d686
ninjagabo.131280 <ninjagabo.131280@gmail.com>
2024-09-17 17:17:59 +0000

    feat: se creo un dto para getDownloadUrl fetch response

---
+++ src/track/dto/get-download-url.dto.ts
@@ -0,0 +1,11 @@
+export interface GetDownloadTrackDto {
+  link?: string;
+  title?: string;
+  filesize?: number;
+  progress?: number;
+  duration?: number;
+  status: string;
+  msg: string;
+  code?: number;
+  error?: string;
+}
