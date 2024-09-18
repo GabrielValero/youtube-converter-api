7b37b07dde404ff7df74caf7776b112149cb5398
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2024-09-17 20:54:10 -0400

    Update: se actualizo la ruta de app.service en track.controller.spec

--- src/track/track.controller.spec.ts
+++ src/track/track.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TrackController } from './track.controller';
+import { AppService } from '../app.service';
 
 describe('TrackController', () => {
   let controller: TrackController;
@@ -7,6 +8,7 @@ describe('TrackController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TrackController],
+      providers: [AppService],
     }).compile();
 
     controller = module.get<TrackController>(TrackController);
