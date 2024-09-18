a3b3b03100761363f4b46f68a02bc7545b46f045
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2024-09-18 01:02:35 -0400

    delete: se migro el codigo a la carpeta track

--- src/youtube-videos/youtube-videos.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { YoutubeVideosController } from './youtube-videos.controller';
-import { YoutubeVideosService } from './youtube-videos.service';
-
-@Module({
-  controllers: [YoutubeVideosController],
-  providers: [YoutubeVideosService]
-})
-export class YoutubeVideosModule {}
