b671ec79918c09b2dc241590baf17799cc85852d
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2024-09-18 01:04:23 -0400

    Update types.d.ts
    Se extendio TrackData y se añadieron objetos de respuesta de la api de youtube

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,50 @@
-export interface TrackData{
-    title: string,
-    artwork: string,
-    videoId: string,
-    artist: string,
-    duration?: string
+export interface TrackData {
+  title: string;
+  artwork: thumbnailType;
+  videoId: string;
+  artist: string;
+  description: string;
+  publishedAt: Date;
+  channelId: string;
+  duration?: string;
 }
+interface SearchListResponse {
+  kind: string;
+  etag: string;
+  nextPageToken: string;
+  prevPageToken: string;
+  regionCode: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: SearchResult[];
+}
+
+interface SearchResult {
+  kind: 'youtube#searchResult';
+  etag: string;
+  id: {
+    kind: string;
+    videoId: string;
+    channelId: string;
+    playlistId: string;
+  };
+  snippet: {
+    publishedAt: Date;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: thumbnailType;
+    channelTitle: string;
+    liveBroadcastContent: string;
+  };
+}
+
+type thumbnailType = {
+    [key: string]: {
+      url: string;
+      width: number;
+      height: number;
+    };
+  }
