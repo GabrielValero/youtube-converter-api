0a1d8fd10e07f9a7449331c302792c4bd7996db0
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2024-09-18 01:03:30 -0400

    Refactor: a replaceSpecialCharacters y a specialCharacters se les cambiaron la extension de .js a .ts

---
+++ src/utils/replaceSpecialCharacters.ts
@@ -0,0 +1,20 @@
+
+import specialCharacters from '../config/specialCharacters.js'
+
+export default function replaceSpecialCharacters(str): string{
+
+    
+    const characters = {specialCharacters}
+    var pattern = /&([a-zA-Z0-9]+);|&#39;/g; 
+    var match = pattern.exec(str) //revisa si el str tiene algun codigo especial
+    
+    while(match != null){ //it repeats untill str gets no special code
+        pattern.lastIndex = 0;
+        
+        //change special codes with special characters
+        str = str.replaceAll(match[0], characters.specialCharacters[match[0].toUpperCase()].characters) 
+        
+        match = pattern.exec(str)
+    }
+    return str
+}
